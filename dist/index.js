689409236fcd8cfc1677bf29ea497f7f9a22e6a7
Lisa Chan <lc131715@gmail.com>
2025-12-31 12:29:42 -0800

    add todo interface

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,30 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
 const list = document.getElementById('todolist');
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    if (!input.value) {
+        alert('Please enter a to-do');
+    }
+    else {
+        const newTodo = {
+            text: input.value,
+            completed: false,
+        };
+        createTodo(newTodo);
+        todos.push(newTodo);
+        input.value = '';
+    }
+}
+function createTodo(todo) {
     const newLi = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     newLi.append(checkbox);
     list.append(newLi);
-    input.value = '';
 }
 form.addEventListener('submit', handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+interface Todo {
+    text: string,
+    completed: boolean,
+}
+
+const todos: Todo[] = [];
+
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
@@ -5,14 +12,26 @@ const list = document.getElementById('todolist')!;
 
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    const newTodoText = input.value;
+    if (!input.value) {
+        alert('Please enter a to-do')
+    } else {
+        const newTodo: Todo = {
+            text: input.value,
+            completed: false,
+        }
+        createTodo(newTodo);
+        todos.push(newTodo);
+        input.value = '';
+    }
+}
+
+function createTodo(todo: Todo) {
     const newLi = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     newLi.append(checkbox);
     list.append(newLi);
-    input.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
