150e4c8324dae8bb9ca766d2432d2309cf5e226a
Lisa Chan <lc131715@gmail.com>
2025-12-30 18:35:43 -0800

    create form

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+const form = document.querySelector('form');
+const list = document.getElementById('todolist');
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = '';
+}
+form.addEventListener('submit', handleSubmit);
